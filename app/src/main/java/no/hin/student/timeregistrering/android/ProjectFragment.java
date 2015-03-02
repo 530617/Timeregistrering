d1690f56153bf9a4b1f5da8abbc4f6b9c3081a75
Aleksander V Grunnvoll <vallegrunnvoll@gmail.com>
2015-03-02 18:58:38 +0100

    Format time string

--- app/src/main/java/no/hin/student/timeregistrering/android/ProjectFragment.java
+++ app/src/main/java/no/hin/student/timeregistrering/android/ProjectFragment.java
@@ -74,7 +74,54 @@ public class ProjectFragment extends Fragment
 
     public void onSecondsUpdate()
     {
-        elapsedSeconds++;
-        clock.setText(String.valueOf(elapsedSeconds)); // må formateres riktig (gjøres ikke nå)
+        String tid = formatTime(++elapsedSeconds);
+        clock.setText(tid);
+    }
+
+    private String formatTime(int elapsedSeconds)
+    {
+        int hours = elapsedSeconds / 3600;
+        int minutes = (elapsedSeconds % 3600) / 60;
+        int seconds = (elapsedSeconds % 60);
+
+        String time = formatHours(hours) + formatMinutes(minutes) + formatSeconds(seconds);
+
+        return time;
+    }
+
+    private String formatHours(int hours)
+    {
+        String hourString = "";
+
+        if (hours < 10)
+            hourString += "0" + hours + ":";
+        else
+            hourString += hours + ":";
+
+        return hourString;
+    }
+
+    private String formatMinutes(int minutes)
+    {
+        String minuteString = "";
+
+        if (minutes < 10)
+            minuteString += "0" + minutes + ":";
+        else
+            minuteString += minutes + ":";
+
+        return minuteString;
+    }
+
+    private String formatSeconds(int seconds)
+    {
+        String secondString = "";
+
+        if (seconds < 10)
+            secondString += "0" + seconds;
+        else
+            secondString += seconds;
+
+        return secondString;
     }
 }
